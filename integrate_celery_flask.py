5568fda479e13355a9bfd172c5192ea98c7f711e
sundbergjohanna <91964818+sundbergjohanna@users.noreply.github.com>
2021-10-10 10:40:43 +0200

    Update integrate_celery_flask.py

--- integrate_celery_flask.py
+++ integrate_celery_flask.py
@@ -5,7 +5,7 @@ from celery import Celery
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
+        backend=app.config['RESULT_BACKEND'],
         broker=app.config['CELERY_BROKER_URL']
     )
     celery.conf.update(app.config)
